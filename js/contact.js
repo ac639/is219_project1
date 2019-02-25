86a39f3efd9be0131ba47898c2c1d62e101ace83
andre <ac639@njit.edu>
2019-02-25 02:17:30 -0500

    Changed response tags to be more friendly

--- js/contact.js
+++ js/contact.js
@@ -35,23 +35,23 @@ $(document).ready(function(){
             },
             messages: {
                 name: {
-                    required: "come on, you have a name, don't you?",
+                    required: "Please enter name",
                     minlength: "your name must consist of at least 2 characters"
                 },
                 subject: {
-                    required: "come on, you have a subject, don't you?",
+                    required: "Please enter subject",
                     minlength: "your subject must consist of at least 4 characters"
                 },
                 number: {
-                    required: "come on, you have a number, don't you?",
+                    required: "Please enter number,
                     minlength: "your Number must consist of at least 5 characters"
                 },
                 email: {
-                    required: "no email, no message"
+                    required: "Please enter e-mail"
                 },
                 message: {
-                    required: "um...yea, you have to write something to send this form.",
-                    minlength: "thats all? really?"
+                    required: "Please write something in the form",
+                    minlength: "Minimum length not reached"
                 }
             },
             submitHandler: function(form) {
